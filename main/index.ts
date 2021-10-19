4a426cc483cb697d00e16975400dc168768df5ab
davidpodhola <david@podhola.net>
2021-10-19 21:47:31 +0200

    works

--- main/index.ts
+++ main/index.ts
@@ -1,13 +1,36 @@
 import { Context, HttpRequest } from '@azure/functions';
 import { AzureHttpAdapter } from '@nestjs/azure-func-http';
-import { createApp, loggerWrapper } from "../src/main.azure";
+import { createApp, loggerWrapper } from '../src/main.azure';
+import { Sequelize } from 'sequelize';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const intercept = require('azure-function-log-intercept');
 
-export default function(context: Context, req: HttpRequest): void {
+export default function (context: Context, req: HttpRequest): void {
   intercept(context); // console.log works now!
   context.log('main/index started');
   (loggerWrapper as any).logger = context.log;
-  AzureHttpAdapter.handle(createApp, context, req);
+  try {
+    /* const sequelize = new Sequelize(
+      process.env.NX_MSSQL_DATABASE,
+      process.env.NX_MSSQL_USER,
+      process.env.NX_MSSQL_PASSWORD,
+      {
+        dialect: 'mssql',
+        host: process.env.NX_MSSQL_HOST,
+        dialectOptions: {
+          // Observe the need for this nested `options` field for MSSQL
+          options: {
+            // Your tedious options here
+            useUTC: false,
+            dateFirst: 1,
+          },
+        },
+      },
+    ); */
+
+    AzureHttpAdapter.handle(createApp, context, req);
+  } catch (e) {
+    context.log.error(e);
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,50 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Sequelize } from 'sequelize';
+
+export const isPgsql = (process.env.NX_DB_DIALECT || 'postgres') === 'postgres';
+export const isMssql = process.env.NX_DB_DIALECT === 'mssql';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+
+    {
+      provide: 'SEQUELIZE',
+      inject: [],
+      useFactory: async () => {
+        let sequelize;
+        if (isPgsql) {
+          sequelize = new Sequelize(
+            process.env.DATABASE_URL ||
+              'postgres://postgres:Coggel1234@localhost:5433/gt2',
+          );
+        } else if (isMssql) {
+          sequelize = new Sequelize(
+            process.env.NX_MSSQL_DATABASE,
+            process.env.NX_MSSQL_USER,
+            process.env.NX_MSSQL_PASSWORD,
+            {
+              dialect: 'mssql',
+              host: process.env.NX_MSSQL_HOST,
+              dialectOptions: {
+                // Observe the need for this nested `options` field for MSSQL
+                options: {
+                  // Your tedious options here
+                  useUTC: false,
+                  dateFirst: 1,
+                },
+              },
+            },
+          );
+        } else throw new Error('Database dialect not supported');
+        // initModels(sequelize);
+        return sequelize;
+      },
+    },
+  ],
 })
 export class AppModule {}
