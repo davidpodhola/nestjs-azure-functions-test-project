1d54a83f6828ba03dafa437482c7f1c17f62e51a
davidpodhola <david@podhola.net>
2021-10-19 19:28:49 +0200

    we have the env

--- src/main.azure.ts
+++ src/main.azure.ts
@@ -8,18 +8,19 @@ export const loggerWrapper = {
 
 export async function createApp(): Promise<INestApplication> {
   const logger = (loggerWrapper as any).logger;
-  logger.verbose('we are starting');
+  logger.info('we are starting');
+  logger.info(process.env);
   const app = await NestFactory.create(AppModule, {
     logger: (loggerWrapper as any).logger,
   });
   app.setGlobalPrefix('api');
 
   try {
-    logger.verbose('app will init');
+    logger.info('app will init');
     await app.init();
-    logger.verbose('app is OK');
+    logger.info('app is OK');
     return app;
   } catch (e) {
-    logger.error('app NOK', e);
+    logger.info('app NOK', e);
   }
 }
